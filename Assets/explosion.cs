2894a0ffbcbc3156acb9f213e27f1376324ec0d8
VALKYRIA\照維 <weiweimpnkey70113@gmail.com>
2017-01-21 07:27:51 +0900

    可執行的動畫加上碰撞器 (但是速度還沒對上&動畫不會停)

---
+++ Assets/explosion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    public GameObject ExplosionAnimation;
+    private int exist = 0;
+   
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    public float explosionSpeed =1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(exist == 0)
+        {
+            Instantiate(ExplosionAnimation);
+            exist = 1;
+        }
+        
+        transform.localScale *= (1 + (explosionSpeed * Time.deltaTime));
+        if (transform.localScale.x > 9f)
+        {
+            DestroyObject(ExplosionAnimation);
+            DestroyObject(this.gameObject);
+        }
+    }
+}
---
+++ Assets/wave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wave : MonoBehaviour
+{
+    public GameObject explosion;
+    public float delaytime = 1;
+    private float deltatime = 0;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        deltatime += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && deltatime >= delaytime)
+        {
+            GameObject waveCopy = null;
+            waveCopy = Instantiate(explosion, transform.position, new Quaternion(0, 0, 0, 0));
+            deltatime = 0;
+        }
+    }
+}
